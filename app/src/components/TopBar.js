36dbac4fa295d830b9dfa5282a31a86d597913e5
mcarillon <mcarillon@chargedevelopment.io>
2022-03-14 12:31:09 -0400

    Update color of links and make clicking name of app go home

--- app/src/components/TopBar.js
+++ app/src/components/TopBar.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './TopBar.css';
+import { Link } from 'react-router-dom';
 
 function TopBar(props) {
 	return (
 		<div className="top-bar">
 			<div className="top-bar__body">
-				<h6>Fitness Tracker</h6>
+				<h6>
+					<Link to="/" className="top-bar__link">
+						Fitness Tracker
+					</Link>
+				</h6>
 				<div className="top-bar__right-align">{props.children}</div>
 			</div>
 		</div>
